a711362e40718f2499c2b1ddf700e87922bd33e4
Gabrielly Baião <103866012+GabriellyBaiao@users.noreply.github.com>
2025-01-25 21:08:51 -0300

    [PTI] feat: Adicionando exceções personalizadas

--- exceptions-java/src/br/com/dio/exceptions/CheckedException.java
+++ exceptions-java/src/br/com/dio/exceptions/CheckedException.java
@@ -1,3 +1,5 @@
+package br.com.dio.exceptions;
+
 import javax.swing.*;
 import java.io.*;
 
---
+++ exceptions-java/src/br/com/dio/exceptions/ImpossivelAberturaDeArquivoException.java
@@ -0,0 +1,29 @@
+package br.com.dio.exceptions;
+
+public class ImpossivelAberturaDeArquivoException extends Exception{
+
+    private String nomeDoArquivo;
+    private String diretorio;
+
+    public ImpossivelAberturaDeArquivoException(String nomeDoArquivo, String diretorio) {
+        super("O arquivo " + nomeDoArquivo + " não foi encontrado no diretório " + diretorio);
+        this.nomeDoArquivo = nomeDoArquivo;
+        this.diretorio = diretorio;
+    }
+
+    public String getNomeDoArquivo() {
+        return nomeDoArquivo;
+    }
+
+    public void setNomeDoArquivo(String nomeDoArquivo) {
+        this.nomeDoArquivo = nomeDoArquivo;
+    }
+
+    public String getDiretorio() {
+        return diretorio;
+    }
+
+    public void setDiretorio(String diretorio) {
+        this.diretorio = diretorio;
+    }
+}
